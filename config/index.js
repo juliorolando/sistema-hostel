d8ea456501af21cd18821fb5b29f85ff1edd9177
Julio Rolando <86392193+juliorolando@users.noreply.github.com>
2022-06-06 03:46:15 -0300

    Update

--- config/index.js
+++ config/index.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 exports.config = {
     db: {
-        user: process.env.DB_USER || 'root',
+        user: process.env.DB_USER || '',
         host: process.env.DB_HOST || 'localhost',
-        password: process.env.DB_PASSWORD || 'jarolando',
+        password: process.env.DB_PASSWORD || '',
         port: process.env.DB_PORT || '3306',
-        database: process.env.DB_NAME || 'hamerica'
+        database: process.env.DB_NAME || ''
     }
 }
